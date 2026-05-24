0f831ac7453250260f818698ccb434620908d5d2
matthewhendrix <matthew.hendrix@geappliances.com>
2026-05-24 10:54:44 -0400

    add unit tests for the material widget updates

--- packages/flutter/lib/src/material/text_form_field.dart
+++ packages/flutter/lib/src/material/text_form_field.dart
@@ -12,7 +12,7 @@ import 'adaptive_text_selection_toolbar.dart';
 import 'input_decorator.dart';
 import 'material_state.dart';
 import 'text_field.dart';
-import 'theme.dart';
+import 'text_selection_theme.dart';
 
 export 'package:flutter/services.dart' show SmartDashesType, SmartQuotesType;
 
@@ -296,7 +296,7 @@ class TextFormField extends FormField<String> {
                mouseCursor: mouseCursor,
                contextMenuBuilder:
                    contextMenuBuilder ??
-                   Theme.of(state.context).textSelectionTheme.contextMenuBuilder ??
+                   TextSelectionTheme.of(state.context).contextMenuBuilder ??
                    _defaultContextMenuBuilder,
                spellCheckConfiguration: spellCheckConfiguration,
                magnifierConfiguration: magnifierConfiguration,
